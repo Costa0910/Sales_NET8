982989bc8265ac9cd063584d617be3aea50b6a79
Armando Costa <83301015+Costa0910@users.noreply.github.com>
2024-09-02 10:06:57 +0100

    CRUD Categories

--- Sales_NET8.Web/obj/Debug/net8.0/Sales_NET8.Web.AssemblyInfo.cs
+++ Sales_NET8.Web/obj/Debug/net8.0/Sales_NET8.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sales_NET8.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61fd1f373fdd94b6e4b7b1586039bf758260c007")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85d000151c362b83700fccac183cac01a8cead7b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sales_NET8.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sales_NET8.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
